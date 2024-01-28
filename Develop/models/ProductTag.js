a732668049d646936a0bfd33c78def1035da54cb
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2024-01-27 21:20:35 -0700

    added models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER, // define type
+      allowNull: false, // does not allow null values
+      primaryKey: true, // set as primary key
+      autoIncrement: true, // uses auto increment
+    },
+    product_id: {
+      type: DataTypes.INTEGER, // define type
+      references: {
+        model: "product", // what model is coming from
+        key: "id", // what key it is coming from
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, //define type
+      references: {
+        model: "tag", //what model is coming from
+        key: "id", //what key its coming from
+      },
+    },
   },
   {
     sequelize,
@@ -18,3 +37,27 @@ ProductTag.init(
 );
 
 module.exports = ProductTag;
+
+// - `ProductTag`
+
+//   - `id`
+
+//     - Integer.
+
+//     - Doesn't allow null values.
+
+//     - Set as primary key.
+
+//     - Uses auto increment.
+
+//   - `product_id`
+
+//     - Integer.
+
+//     - References the `Product` model's `id`.
+
+//   - `tag_id`
+
+//     - Integer.
+
+//     - References the `Tag` model's `id`.
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,18 +4,30 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // The `/api/products` endpoint
 
 // get all products
-router.get("/", (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
-});
+router.get("/", async (req, res) => {
+  try {
+   const products = await Product.findAll({ // find all products
+    // be sure to include its associated Category and Tag data
+    include: [{ model: Category }, {model: Tag }],  
+   });
+     res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ message: "Products not found" })
+  }
+  });
+
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+  try {
+    include: [{ model: Category }, { model: Tag}],
+    const product = await Product.findByPk(req.params.id, {   // associated Category and Tag data
+      
+   });     
+}); 
 
-// create new product
+// create new product    
 router.post("/", (req, res) => {
   /* req.body should look like this...
     {
